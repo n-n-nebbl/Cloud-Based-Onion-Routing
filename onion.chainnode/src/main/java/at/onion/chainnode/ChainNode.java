7d3911c7e1108e76f378d2e78c82f4e9a5d7c3b8
m.nebel <alex.nebel@gmail.com>
2014-12-02 20:29:57 +0100

    added dependency for dirNodeClient. chainNode is registering at dirNode at startup through -DdirNode.hostname property

--- onion.chainnode/src/main/java/at/onion/chainnode/ChainNode.java
+++ onion.chainnode/src/main/java/at/onion/chainnode/ChainNode.java
@@ -1,24 +1,52 @@
 package at.onion.chainnode;
 
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import at.onion.commons.CryptoUtils;
+import at.onion.commons.NodeInfo;
+import at.onion.directorynodeClient.CoreClient;
+import at.onion.directorynodeClient.SimpleCoreClient;
+
 public class ChainNode {
 
-	private static final int DEFAULT_PORT = 9000;
-			
-	private static Logger	logger	= LoggerFactory.getLogger(ChainNode.class);
+	private static final int	DEFAULT_PORT	= 9000;
+
+	private static Logger		logger			= LoggerFactory.getLogger(ChainNode.class);
 
 	public static void main(String[] args) {
 		ConnectionService connServ = new ConnectionService();
-		
+
 		int port = DEFAULT_PORT;
 		try {
-			port = Integer.parseInt(args[0]);
+			port = Integer.parseInt(System.getProperty("chainNode.port"));
 		} catch (Exception e) {
-			logger.info("Could not find/parse port, using default port {}", DEFAULT_PORT);
 		}
-		
+
+		String dirNodeHostname = System.getProperty("dirNode.hostname");
+
+		if (dirNodeHostname == null) {
+			logger.warn("No directoryNode Hostname found --> no connection to dirNode possible");
+		} else {
+			try {
+				String localAdress = getLocalIPAdress();
+				CoreClient dirNodeClient = new SimpleCoreClient(InetAddress.getByName(dirNodeHostname), 8001);
+				dirNodeClient.addNode(new NodeInfo(localAdress, port, CryptoUtils.getKeyPair().getPublic()));
+				logger.info("successfully registered at directoryNode");
+			} catch (UnknownHostException e) {
+				logger.error("Could not resolve Hostname", e);
+			} catch (Exception e) {
+				logger.error("Error adding Node to directory Server", e);
+			}
+		}
+
 		try {
 			connServ.startListening(port);
 		} catch (Exception e) {
@@ -27,4 +55,32 @@ public class ChainNode {
 		}
 	}
 
+	private static String getLocalIPAdress() throws SocketException {
+		Enumeration<NetworkInterface> e = NetworkInterface.getNetworkInterfaces();
+		while (e.hasMoreElements()) {
+			NetworkInterface n = e.nextElement();
+			Enumeration<InetAddress> ee = n.getInetAddresses();
+			while (ee.hasMoreElements()) {
+				InetAddress i = ee.nextElement();
+				logger.debug("ip found {}", i.getHostAddress());
+				if (i instanceof Inet6Address) {
+					continue;
+				}
+
+				// filter wrong adresses
+				int first = Integer.parseInt(i.getHostAddress().split("\\.")[0]);
+				int second = Integer.parseInt(i.getHostAddress().split("\\.")[1]);
+				if (first != 127) {
+					if (!(first == 172 && second == 16)) {
+						if (!(first == 172 && second == 31)) {
+							if (!(first == 169 && second == 254)) {
+								return i.getHostAddress();
+							}
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
 }
